5da43110a54b0dab9910489c7d33749f30d54fd0
John Vickers <jgvicke@gmail.com>
2018-04-28 00:45:43 -0400

    Initial Release

---
+++ ExampleGraph.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace DirectPointEdit
+{
+    partial class ExampleGraph
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.curveIndex = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.step = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.up = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.right = new System.Windows.Forms.Button();
+            this.down = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.curveIndex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.step)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.28117F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.71883F));
+            this.tableLayoutPanel1.Controls.Add(this.zedGraphControl1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(754, 260);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.zedGraphControl1.Location = new System.Drawing.Point(3, 3);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(538, 254);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.MouseDownEvent += new ZedGraph.ZedGraphControl.ZedMouseEventHandler(this.ZedGraphControl1_MouseDownEvent);
+            this.zedGraphControl1.MouseUpEvent += new ZedGraph.ZedGraphControl.ZedMouseEventHandler(this.zedGraphControl1_MouseUpEvent);
+            this.zedGraphControl1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.zedGraphControl1_MouseMove);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.curveIndex, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.step, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(547, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(204, 254);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Point Index";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // curveIndex
+            // 
+            this.curveIndex.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.curveIndex.Location = new System.Drawing.Point(69, 3);
+            this.curveIndex.Name = "curveIndex";
+            this.curveIndex.Size = new System.Drawing.Size(132, 20);
+            this.curveIndex.TabIndex = 1;
+            this.curveIndex.ValueChanged += new System.EventHandler(this.curveIndex_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Step Size";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // step
+            // 
+            this.step.DecimalPlaces = 2;
+            this.step.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.step.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.step.Location = new System.Drawing.Point(69, 29);
+            this.step.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.step.Name = "step";
+            this.step.Size = new System.Drawing.Size(132, 20);
+            this.step.TabIndex = 2;
+            this.step.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 3;
+            this.tableLayoutPanel2.SetColumnSpan(this.tableLayoutPanel3, 2);
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Controls.Add(this.up, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.left, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.right, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.down, 1, 2);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 55);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(198, 196);
+            this.tableLayoutPanel3.TabIndex = 3;
+            // 
+            // up
+            // 
+            this.up.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.up.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.up.Location = new System.Drawing.Point(68, 3);
+            this.up.Name = "up";
+            this.up.Size = new System.Drawing.Size(59, 59);
+            this.up.TabIndex = 0;
+            this.up.UseVisualStyleBackColor = true;
+            this.up.Click += new System.EventHandler(this.up_Click);
+            // 
+            // left
+            // 
+            this.left.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.left.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.left.Location = new System.Drawing.Point(3, 68);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(59, 59);
+            this.left.TabIndex = 0;
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // right
+            // 
+            this.right.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.right.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.right.Location = new System.Drawing.Point(133, 68);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(62, 59);
+            this.right.TabIndex = 0;
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // down
+            // 
+            this.down.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.down.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.down.Location = new System.Drawing.Point(68, 133);
+            this.down.Name = "down";
+            this.down.Size = new System.Drawing.Size(59, 60);
+            this.down.TabIndex = 0;
+            this.down.UseVisualStyleBackColor = true;
+            this.down.Click += new System.EventHandler(this.down_Click);
+            // 
+            // ExampleGraph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 260);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "ExampleGraph";
+            this.Text = "Example Graph";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.curveIndex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.step)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown curveIndex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown step;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button up;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button down;
+    }
+}
+
---
+++ ExampleGraph.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace DirectPointEdit
+{
+    public partial class ExampleGraph : Form
+    {
+        private GraphPane _myPane;
+        private PointPairList _points;
+        private LineItem _myCurve;
+
+        private bool mouseDown = false;
+        private int numPoints = 10;
+        private double _sinPart;
+        private double _timePart;
+        private PointPair lastMousePoint;
+
+        public ExampleGraph()
+        {
+            InitializeComponent();
+
+            // get arrow resourse
+            var _assembly = Assembly.GetExecutingAssembly();
+            var _imageStream = _assembly.GetManifestResourceStream("DirectPointEdit.DownArrow.png");
+            down.BackgroundImage = new Bitmap(_imageStream);
+            up.BackgroundImage = new Bitmap(_imageStream);
+            up.BackgroundImage.RotateFlip(RotateFlipType.Rotate180FlipNone);
+            right.BackgroundImage = new Bitmap(_imageStream);
+            right.BackgroundImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            left.BackgroundImage = new Bitmap(_imageStream);
+            left.BackgroundImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+            _timePart = 1.0 / numPoints;
+            _sinPart = 2.0 * Math.PI / numPoints;
+
+            _myPane = zedGraphControl1.GraphPane;
+            _myPane.Legend.IsVisible = false;
+            _myPane.Title.Text = "Moveable Points";
+            _myPane.XAxis.Title.Text = "X";
+            _myPane.YAxis.Title.Text = "Y";
+            
+            _myPane.YAxis.Scale.Min = -1.2;
+            _myPane.YAxis.Scale.Max = 1.2;
+            _myPane.YAxis.Scale.MajorStep = 1;
+            _myPane.YAxis.Scale.MinorStep = 1;
+
+            _myPane.XAxis.Scale.Min = -0.2;
+            _myPane.XAxis.Scale.Max = 1.2;
+            _myPane.XAxis.Scale.MajorStep = 1;
+            _myPane.XAxis.Scale.MinorStep = 1;
+
+            _points = new PointPairList();
+            _myCurve = _myPane.AddCurve("My Points", _points, Color.Blue, SymbolType.Circle);
+            _myCurve.Symbol.Fill = new Fill(Color.Blue, Color.Red);
+            _myCurve.Symbol.Fill.Type = FillType.GradientByZ;
+            _myCurve.Symbol.Fill.SecondaryValueGradientColor = Color.Empty;
+            _myCurve.Symbol.Size = 15;
+            _myCurve.Symbol.Fill.IsVisible = true;
+            _myCurve.Symbol.Border.IsVisible = false;
+
+            for (int i = 0; i <= numPoints; i++)
+            {
+                _points.Add(i * _timePart, Math.Sin(i * _sinPart), 0);
+            }
+
+            curveIndex.Maximum = numPoints;
+            curveIndex.Value = 2;
+            HighlightIndex(2);
+        }
+
+        private void RedrawGraph()
+        {
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+        }
+
+        private void HighlightIndex(int index)
+        {
+            // clear any current highlighting
+            _points.Where(point => point.Z == 1).ToList().ForEach(point => point.Z = 0);
+
+            // highlight new point
+            _points[index].Z = 1;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+
+        private void curveIndex_ValueChanged(object sender, EventArgs e)
+        {
+            // highlight the index
+            HighlightIndex((int)curveIndex.Value);
+        }
+        
+        private bool ZedGraphControl1_MouseDownEvent(ZedGraphControl sender, MouseEventArgs e)
+        {
+            // find the closest point to the click location
+            _myPane.FindNearestObject(new PointF(e.X, e.Y), this.CreateGraphics(), out object nearestobject, out int index);
+
+            // highlight the index
+            HighlightIndex(index);
+
+            // update the point value boxes
+            curveIndex.Value = index;
+
+            // get current mouse position
+            lastMousePoint = GetMouseCoords();
+
+            // flag that we have the mouse down
+            mouseDown = true;
+
+            // return complete
+            return true;
+        }
+
+        private bool zedGraphControl1_MouseUpEvent(ZedGraphControl sender, MouseEventArgs e)
+        {
+            // flag that the mouse is up
+            mouseDown = false;
+
+            // perform final move
+            MovePointByMouse();
+            return true;
+        }
+
+        private void zedGraphControl1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+                MovePointByMouse();
+        }
+
+        private PointPair GetMouseCoords()
+        {
+            _myPane.ReverseTransform(Cursor.Position, out double x, out double y);
+            return new PointPair(x, y);
+        }
+
+        private void MovePointByMouse()
+        {
+            var currentMousePoint = GetMouseCoords();
+
+            // get the delta between last mouse move and now
+            var deltaPoint = new PointPair(currentMousePoint.X - lastMousePoint.X, currentMousePoint.Y - lastMousePoint.Y);
+
+            // apply the point to the selected point
+            _points[(int)curveIndex.Value].X += deltaPoint.X;
+            _points[(int)curveIndex.Value].Y += deltaPoint.Y;
+
+            // update last mouse point
+            lastMousePoint = currentMousePoint;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+
+        private void up_Click(object sender, EventArgs e)
+        {
+            _points[(int)curveIndex.Value].Y += (double)step.Value;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+
+        private void right_Click(object sender, EventArgs e)
+        {
+            _points[(int)curveIndex.Value].X += (double)step.Value;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+
+        private void down_Click(object sender, EventArgs e)
+        {
+            _points[(int)curveIndex.Value].Y -= (double)step.Value;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+
+        private void left_Click(object sender, EventArgs e)
+        {
+            _points[(int)curveIndex.Value].X -= (double)step.Value;
+
+            // redraw the graph
+            RedrawGraph();
+        }
+    }
+}
